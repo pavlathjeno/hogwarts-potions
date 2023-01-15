e189276eb2628b6bc77e6b868f43a4cc09741292
pavlathjeno <pavlathjeno@gmail.com>
2023-01-15 15:59:42 +0100

    create : recipe model

---
+++ Models/Entities/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public Student Student { get; set; }
+        public HashSet<Ingredient> Ingredients { get; set; }
+    }
+}
