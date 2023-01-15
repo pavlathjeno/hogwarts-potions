b3ba7dcb031c67b96e2a93ceaa2ea2830bff079c
pavlathjeno <pavlathjeno@gmail.com>
2023-01-15 16:03:29 +0100

    update : auto increment identitys

--- Models/Entities/Ingredient.cs
+++ Models/Entities/Ingredient.cs
@@ -1,9 +1,14 @@
-﻿namespace HogwartsPotions.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Models.Entities
 {
     public class Ingredient
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
+        
 
     }
 }
--- Models/Entities/Recipe.cs
+++ Models/Entities/Recipe.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HogwartsPotions.Models.Entities
 {
     public class Recipe
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public string Name { get; set; }
         public Student Student { get; set; }
